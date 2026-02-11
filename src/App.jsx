ee36530c8e3c7e87371460830a62f21059d8da75
Nesma Salim <nesmasalim66@gmail.com>
2026-02-11 22:55:45 +0530

    Fixed routing using HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import PrivateRoute from "./routes/PrivateRoute";
 
 function App() {
   return (
-    <BrowserRouter>
       
       <Routes>
         <Route path="/" element={<Login />} />    
@@ -25,7 +24,7 @@ function App() {
         <Route path="/editor" element={<ResumeEditor />} />
         <Route path="/preview" element={<Preview />} />
       </Routes>
-    </BrowserRouter>
+    
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext';
+import { HashRouter } from "react-router-dom";
 
 
 createRoot(document.getElementById('root')).render(
    <StrictMode>
     <AuthProvider>
+      <HashRouter>
       <App />
+      </HashRouter>
     </AuthProvider>
   </StrictMode>
 )
